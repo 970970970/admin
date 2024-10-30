b1a89a5029f0f6897de4e0c60ecc2567fa36f2b3
siqi <masiqi@gmail.com>
2024-10-31 00:07:24 +0800

    修复语言列表中的switch开关

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const reload = require('reload');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const { exec } = require('child_process');
@@ -11,7 +10,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || '0.0.0.0');
 app.use(logger('dev'));
-app.use(bodyParser.json()); // Parses json, multi-part (file), url-encoded
+app.use(bodyParser.json());
 
 app.use('/public', express.static('public'));
 app.use('/pages', express.static('pages'));
@@ -41,14 +40,10 @@ exec('node build.js', (error, stdout, stderr) => {
     console.error(`构建错误: ${stderr}`);
   }
 
-  // 构建完成后启动服务器
-  reload(app).then(function (reloadReturned) {
-    server.listen(app.get('port'), app.get('host'), function () {
-      console.log(
-        'Web server listening on port http://' + app.get('host') + ':' + app.get('port')
-      );
-    });
-  }).catch(function (err) {
-    console.error('Reload could not start, could not start server/sample app', err);
+  // 直接启动服务器，不使用reload
+  server.listen(app.get('port'), app.get('host'), function () {
+    console.log(
+      'Web server listening on port http://' + app.get('host') + ':' + app.get('port')
+    );
   });
 });
